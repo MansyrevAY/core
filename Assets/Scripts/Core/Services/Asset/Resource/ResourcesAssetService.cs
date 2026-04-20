f9d8ca787823144d3612f259d8f4841f326a393d
mansyrevam <mansyrevarsenij@gmail.com>
2026-04-20 20:44:34 +0300

    add: handles for resource service

--- Assets/Scripts/Core/Services/Asset/Resource/ResourcesAssetService.cs
+++ Assets/Scripts/Core/Services/Asset/Resource/ResourcesAssetService.cs
@@ -1,54 +1,104 @@
-﻿using System.Collections.Generic;
-using Core.Infra.Service;
+﻿using System;
+using System.Collections.Generic;
+using Core.Infra.Log;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using IServiceProvider = Core.Infra.Service.IServiceProvider;
+using Object = UnityEngine.Object;
 
 namespace Core.Services.Asset.Resource
 {
     public class ResourcesAssetService : IAssetService
     {
-        private readonly Dictionary<string, Object> _assets = new();
+        private readonly Dictionary<string, ResourceAssetHandle> _handles = new();
         
         public void Init(IServiceProvider provider)
         {
         }
 
-        public async UniTask<T> Instantiate<T>(string id) where T : Object
+        public async UniTask<GameObject> Instantiate(string id)
         {
-            if (!_assets.TryGetValue(id, out var asset))
+            var handle = await GetHandle(id);
+            handle.AddInstance();
+            
+            _handles.TryAdd(id, handle);
+            
+            return Object.Instantiate(handle.Asset) as GameObject;
+        }
+
+        public async UniTask<T> Instantiate<T>(string id) where T : MonoBehaviour
+        {
+            var handle = await GetHandle(id);
+            handle.AddInstance();
+            
+            _handles.TryAdd(id, handle);
+            
+            var instance = Object.Instantiate(handle.Asset) as GameObject;
+            var script = instance.GetComponent<T>();
+
+            if (!script)
             {
-                asset = await Load<T>(id);
+                throw new NullReferenceException($"GameObject {id} is missing {nameof(T)}");
+            }
+            
+            return script;
+        }
+
+        private async UniTask<ResourceAssetHandle> GetHandle(string id)
+        {
+            if (_handles.TryGetValue(id, out var handle))
+            {
+                return handle;
             }
 
-            if (asset is MonoBehaviour behaviour)
-            {
-                var instance = Object.Instantiate(behaviour.gameObject);
-                return instance.GetComponent<T>();
-            }
-            
-            return Object.Instantiate(asset) as T;
+            var asset = await Resources.LoadAsync<GameObject>(id);
+            handle = new ResourceAssetHandle(asset, 0);
+            return handle;
         }
         
-        public UniTask<T> Load<T>(string id) where T : Object
+        public async UniTask<T> Load<T>(string id) where T : Object
         {
-            if (_assets.TryGetValue(id, out var asset))
+            if (_handles.TryGetValue(id, out var handle))
             {
-                return UniTask.FromResult(asset as T);
+                return handle.Asset as T;
             }
+
+            var asset = await Resources.LoadAsync<GameObject>(id);
+            handle = new ResourceAssetHandle(asset, 1);
+            _handles.Add(id, handle);
             
-            asset = Resources.Load<T>(id);
-            
-            _assets[id] = asset;
-            return UniTask.FromResult((T) asset);
+            return handle.Asset as T;
         }
 
         public void Destroy<T>(IAsset<T> asset) where T : Object
         {
+            if (!_handles.TryGetValue(asset.Id, out var handle))
+            {
+                throw new ArgumentOutOfRangeException(nameof(asset.Id), $"No handle found for asset {asset.Id}");
+            }
+            
+            handle.RemoveInstance();
             Object.Destroy(asset.Instance);
+
+            if (handle.UnloadRequested)
+            {
+                Unload(asset);
+            }
         }
 
         public void Unload<T>(IAsset<T> asset) where T : Object
         {
+            if (!_handles.TryGetValue(asset.Id, out var handle))
+            {
+                throw new ArgumentOutOfRangeException(nameof(asset.Id), $"No handle found for asset {asset.Id}");
+            }
+
+            if (handle.Count > 0)
+            {
+                Log.Warning($"There are {handle.Count} undestroyed instances of {asset.Id}, unexpected behavior might follow");
+            }
+            
+            _handles.Remove(asset.Id);
             Resources.UnloadAsset(asset.Instance);
         }
     }
